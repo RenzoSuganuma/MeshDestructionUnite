cff37c4f0ab60a4ee2459df2c2d5cb57a0a5a77d
DELL_LapTop_RenzoSuganuma <discoverygworks2023@gmail.com>
2024-08-27 11:21:10 +0900

    [ add , update ] ボロノイ キューブの生成に成功。

--- Assets/Scripts/LearnVolonoi/VoronoiMaker_2D.cs
+++ Assets/Scripts/LearnVolonoi/VoronoiMaker_2D.cs
@@ -6,29 +6,30 @@ using Random = UnityEngine.Random;
 // 参考文献
 // https://qiita.com/gis/items/c1762d9683355339ee07
 // https://taq.hatenadiary.jp/entry/2022/12/25/160000#%E6%AD%A3%E6%94%BB%E6%B3%95
-public struct Point
+
+public struct Point2D
 {
     public int x, y;
 }
 
-public static class VoronoiUtil
+public static class VoronoiUtil2D
 {
     public const int MAP_X = 64;
     public const int MAP_Y = 64;
     public const int VORONOI_COUNT_MAX = 1;
 
     // 点の座標と渡される座標の差分の二乗を返す
-    public static int DistanceSqrt_D(Point point, int x, int y)
+    public static int DistanceSqrt_D(Point2D point2D, int x, int y)
     {
-        int xd = x - point.x;
-        int yd = y - point.y;
+        int xd = x - point2D.x;
+        int yd = y - point2D.y;
         return (xd * xd) + (yd * yd);
     }
 }
 
-public class Voronoi
+public class Voronoi2D
 {
-    private List<Point> _points = new();
+    private List<Point2D> _points = new();
     private List<Color> _colors = new();
 
     public void Make(int count)
@@ -41,11 +42,11 @@ public class Voronoi
     // 点と色を配列へ追加
     void CreatePointAndColor(int count)
     {
-        int w = VoronoiUtil.MAP_X - 20;
-        int h = VoronoiUtil.MAP_Y - 20;
+        int w = VoronoiUtil2D.MAP_X - 20;
+        int h = VoronoiUtil2D.MAP_Y - 20;
         for (int i = 0; i < count; i++)
         {
-            var pnt = new Point();
+            var pnt = new Point2D();
             pnt.x = Random.Range(0, w) + 10;
             pnt.y = Random.Range(0, h) + 10;
             var c = new Color(Random.Range(0f, 1.0f), Random.Range(0f, 1.0f), Random.Range(0f, 1.0f));
@@ -56,7 +57,7 @@ public class Voronoi
 
     void CreateSites()
     {
-        int w = VoronoiUtil.MAP_X, h = VoronoiUtil.MAP_Y, d, ind, dmin;
+        int w = VoronoiUtil2D.MAP_X, h = VoronoiUtil2D.MAP_Y, d, ind, dmin;
         for (int hh = 0; hh < h; hh++)
         {
             for (int ww = 0; ww < w; ww++)
@@ -65,12 +66,12 @@ public class Voronoi
                 dmin = Int32.MaxValue;
                 for (int it = 0; it < _points.Count; it++)
                 {
-                    Point p = _points[it];
-                    d = VoronoiUtil.DistanceSqrt_D(p, ww, hh);
+                    Point2D p = _points[it];
+                    d = VoronoiUtil2D.DistanceSqrt_D(p, ww, hh);
                     if (d < dmin) // 
                     {
-                        dmin = d; // 一番近い母店との距離
-                        ind = it; // 一番近い母店の添え字
+                        dmin = d; // 一番近い母点との距離
+                        ind = it; // 一番近い母点の添え字
                     }
                 }
 
@@ -105,7 +106,7 @@ public class Voronoi
 
 public class VoronoiMaker_2D : MonoBehaviour
 {
-    private Voronoi v = new Voronoi();
+    private Voronoi2D v = new Voronoi2D();
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
---
+++ Assets/Scripts/LearnVolonoi/VoronoiMaker_3D.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+// 参考文献
+// https://qiita.com/gis/items/c1762d9683355339ee07
+// https://taq.hatenadiary.jp/entry/2022/12/25/160000#%E6%AD%A3%E6%94%BB%E6%B3%95
+
+public struct Point3D
+{
+    public int x, y, z;
+}
+
+public static class VoronoiUtil3D
+{
+    public const int MAP_XYZ = 32;
+    public const int VORONOI_COUNT_MAX = 1;
+
+    // 点の座標と渡される座標の差分の二乗を返す
+    public static int DistanceSqrt_D3D(Point3D point3D, int x, int y, int z)
+    {
+        int xd = x - point3D.x;
+        int yd = y - point3D.y;
+        int zd = z - point3D.z;
+        return (xd * xd) + (yd * yd) + (zd * zd);
+    }
+}
+
+public class Voronoi3D
+{
+    private List<Point3D> _points = new();
+    private List<Color> _colors = new();
+
+    public void Make(int count)
+    {
+        CreatePointAndColor3D(count);
+        CreateSites3D();
+        SetSitesPoints3D();
+    }
+
+    // 点と色を配列へ追加
+    void CreatePointAndColor3D(int count)
+    {
+        int mapX = VoronoiUtil3D.MAP_XYZ - 20;
+        int mapY = VoronoiUtil3D.MAP_XYZ - 20;
+        int mapZ = VoronoiUtil3D.MAP_XYZ - 20;
+        for (int i = 0; i < count; i++)
+        {
+            var pnt = new Point3D();
+            pnt.x = Random.Range(0, mapX) + 10;
+            pnt.y = Random.Range(0, mapY) + 10;
+            pnt.z = Random.Range(0, mapZ) + 10;
+            var c = new Color(Random.Range(0f, 1.0f), Random.Range(0f, 1.0f), Random.Range(0f, 1.0f));
+            _points.Add(pnt);
+            _colors.Add(c);
+        }
+    }
+
+    void CreateSites3D()
+    {
+        int mapX = VoronoiUtil3D.MAP_XYZ, mapY = VoronoiUtil3D.MAP_XYZ, mapZ = VoronoiUtil3D.MAP_XYZ, d, ind, dmin;
+        for (int dd = 0; dd < mapZ; dd++)
+        {
+            for (int hh = 0; hh < mapY; hh++)
+            {
+                for (int ww = 0; ww < mapX; ww++)
+                {
+                    ind = -1;
+                    dmin = Int32.MaxValue;
+                    for (int it = 0; it < _points.Count; it++)
+                    {
+                        Point3D p = _points[it];
+                        d = VoronoiUtil3D.DistanceSqrt_D3D(p, ww, hh, dd);
+                        if (d < dmin) // 
+                        {
+                            dmin = d; // 一番近い母点との距離
+                            ind = it; // 一番近い母点の添え字
+                        }
+                    }
+
+                    if (ind > -1)
+                    {
+                        var obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                        obj.transform.position = new Vector3(ww, hh, dd);
+                        obj.GetComponent<MeshRenderer>().material.color = _colors[ind];
+                    }
+                }
+            }
+        }
+    }
+
+    // 母点を描写
+    void SetSitesPoints3D()
+    {
+        foreach (var point in _points)
+        {
+            int x = point.x, y = point.y, z = point.z;
+
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    for (int k = -1; k < 2; k++)
+                    {
+                        var obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                        obj.transform.position = new Vector3(x + i, y + j, z + k);
+                        obj.GetComponent<MeshRenderer>().material.color = Color.black;
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+/// <summary>
+/// ボロノイ キューブを生成する
+/// </summary>
+public class VoronoiMaker_3D : MonoBehaviour
+{
+    private Voronoi3D diagram = new();
+
+    private void Start()
+    {
+        diagram.Make(VoronoiUtil3D.MAP_XYZ);
+    }
+}
